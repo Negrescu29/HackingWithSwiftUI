1ef604ca16175b2a7123c321b43c9bf302bb813c
Ion <ion.negrescu@rubicoin.com>
2019-09-23 15:53:40 +0100

    Day 1 of 100

---
+++ Playground/20190923_Day1.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+// VARIABLE
+
+var str = "Hello, playground"
+
+var age = 48
+
+var population = 8_000_000
+
+//STRINGS
+
+// Triple """ should be on the single loine
+var multilineStrings = """
+This
+is
+a
+multiline
+String
+"""
+
+var backslashString = """
+
+This \
+is \
+a \
+different \
+way \
+of \
+multi line string
+"""
+
+//DOUBLE & BOULEANS
+
+var pi = 3.14
+
+var awesome = true
+
+// STRING INTERPOLATION
+
+var score = 85
+var message = "Your score was\(score)"
+
+//CONSTANTS
+
+let taylor = "Swift"
+
+//TYPE ANNOTATIONS - swift know how to find out what type your var,let are but you can also declare it 
+
+let name: String = "Ion"
+let year: Int = 1994
+let height: Double = 1.87
+let isMale: Bool = true
