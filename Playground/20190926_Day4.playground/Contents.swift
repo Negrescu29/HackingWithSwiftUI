f50ba7ba3632ebe93123c855557fdb611b8c4ae6
negrescu29 <ion.negrescu94@gmail.com>
2019-09-26 23:54:16 +0100

    EXITING MULTIPLE LOOP - nested loop

--- Playground/20190926_Day4.playground/Contents.swift
+++ Playground/20190926_Day4.playground/Contents.swift
@@ -100,3 +100,18 @@ while countDown >= 0 {
 
     countDown -= 1
 }
+
+print("")
+//EXITING MULTIPLE LOOP -> loop in a loop is called nested loop
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
