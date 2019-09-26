104a93b7d4ee107cfe223dd6a3441986d4383e58
negrescu29 <ion.negrescu94@gmail.com>
2019-09-26 23:03:23 +0100

    For loops

---
+++ Playground/20190926_Day4.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//FOR LOOPS - most common loops
+
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let culori = ["Alb", "Negru", "Bordo"]
+
+for culoare in culori {
+    print("Culorile mele preferaste sunt: \(culoare)")
+}
+
+print("Players gonna")
+
+for _ in 1...5 {
+    print("play")
+}
+
+
+for _ in 0...3 {
+    print("Hip hip hurray!")
+}
