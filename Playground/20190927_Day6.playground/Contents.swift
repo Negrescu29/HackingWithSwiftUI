fb22d639d36f5801d3b0b16883957ab377332761
negrescu29 <ion.negrescu94@gmail.com>
2019-09-28 22:31:08 +0100

    Returning values from a closure

--- Playground/20190927_Day6.playground/Contents.swift
+++ Playground/20190927_Day6.playground/Contents.swift
@@ -28,3 +28,43 @@ var sendMessage = { (message: String) in
         print("Your message was empty.")
     }
 }
+
+//Returning values from a closure
+
+//cloosure without return
+let commuting = { (place: String) in
+    print("I'm going to \(place) by bus")
+}
+commuting("Blanchardstown")
+
+//closure with return
+let commutingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) by bus"
+}
+print(commutingWithReturn("City Centre"))
+
+let hobby = { (myHobby: String...) in
+    print("My hobby are: \(myHobby)")
+}
+hobby("Judo", "Hicking","Rugby")
+
+let returnMyHobby = {(hobby: String) -> String in
+    return "My favourite hobby is: \(hobby)"
+}
+print(returnMyHobby("Judo"))
+
+let measureSize = { (inches: Int) -> String in
+    switch inches {
+    case 0...26:
+        return "XS"
+    case 27...30:
+        return "S"
+    case 31...34:
+        return "M"
+    case 35...38:
+        return "L"
+    default:
+        return "XL"
+    }
+}
+measureSize(36)
