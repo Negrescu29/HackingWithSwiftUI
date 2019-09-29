da30621676e013d9a9c24c5b1c7beccfb157111d
negrescu29 <ion.negrescu94@gmail.com>
2019-09-29 21:44:14 +0100

    More example for Using closures as parameters when they accept parameters

--- Playground/20190929_ Day 7.playground/Contents.swift
+++ Playground/20190929_ Day 7.playground/Contents.swift
@@ -14,13 +14,57 @@ travel { (place: String) in
 print("")
 
 func adresa (strada: (String) -> Void){
-    print("5")
+    print("Numarul casei: 5")
     strada("Ashfield Park")
-    print("D15 W83F")
+    print("Codul Postal: D15 W83F")
 }
 
 adresa {(place: String) in
-    print("\(place)")
+    print("Numele strazii: \(place)")
 }
 
 
+func fix(item: String, payBill: (Int) -> Void) {
+    print("I've fixed your \(item)")
+    payBill(450)
+}
+
+fix(item: "roof") { (bill: Int) in
+    print("You want $\(bill) for that? Outrageous!")
+}
+
+func getDirections(to destination: String, then travel: ([String]) -> Void) {
+    let directions = [
+        "Go straight ahead",
+        "Turn left onto Station Road",
+        "Turn right onto High Street",
+        "You have arrived at \(destination)"
+    ]
+    travel(directions)
+}
+getDirections(to: "London") { (directions: [String]) in
+    print("I'm getting my car.")
+    for direction in directions {
+        print(direction)
+    }
+}
+
+func getMeasurement(handler: (Double) -> Void) {
+    let measurement = 32.2
+    handler(measurement)
+}
+getMeasurement { (measurement: Double) in
+    print("It measures \(measurement).")
+}
+
+func processPrimes(using closure: (Int) -> Void) {
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19]
+    for prime in primes {
+        closure(prime)
+    }
+}
+processPrimes { (prime: Int) in
+    print("\(prime) is a prime number.")
+    let square = prime * prime
+    print("\(prime) squared is \(square)")
+}
