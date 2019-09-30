bef586abfd19974d150df0672b094e0d630a8af4
Ion <ion.negrescu@rubicoin.com>
2019-09-30 18:30:02 +0100

    Computed properties
    - A property that runs code to figure out its value.
    - Constants cannot be computed properties.
    - Computed property must always have an explicit type.
        `var laptopCondition: String {}`

--- Playground/20190930_Day8.playground/Contents.swift
+++ Playground/20190930_Day8.playground/Contents.swift
@@ -37,3 +37,66 @@ struct RubiksCube {
     var size = 3
 }
 let large = RubiksCube(size: 5)
+
+// Computed properties - a property that runs code to figure out its value.
+struct Laptop {
+    var brand: String
+    var colour: String
+    var screensize: Int
+    var isNewDevice: Bool
+    
+    // Computed proprety must always have an explicit type.
+    var laptopCondition: String {
+        if isNewDevice {
+            return "\(brand) is a new device"
+        }else{
+            return "\(brand) is a second hand device"
+        }
+    }
+    
+}
+let macBook = Laptop(brand: "Apple", colour: "Space Gray", screensize: 15, isNewDevice: true)
+print(macBook.laptopCondition)
+
+
+struct OlimpicSports {
+    var name: String
+    var isOlympicSport: Bool
+    
+    // Constants cannot be computed properties
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+let chessBoxing = OlimpicSports(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+
+struct Medicine {
+    var amount: Int
+    var frequency: Int
+    var dosage: String {
+        return "Take \(amount) pills \(frequency) times a day."
+    }
+}
+
+struct Investor {
+    var age: Int
+    var investmentPlan: String {
+        if age < 30 {
+            return "Shares"
+        } else if age < 60 {
+            return "Equities"
+        } else {
+            return "Bonds"
+        }
+    }
+}
+let investor = Investor(age: 38)
+print(investor.investmentPlan)
+
+
