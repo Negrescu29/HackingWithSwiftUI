d20fcf49a096d5bbe884bcee1cd8b64e9e0db759
Ion <ion.negrescu@rubicoin.com>
2019-10-02 22:34:43 +0100

    Class inheritance
    - You can create a class based on an existing class – it inherits all the properties and methods of the original class, and can add its own on top

--- Playground/20190930_Day10.playground/Contents.swift
+++ Playground/20190930_Day10.playground/Contents.swift
@@ -3,7 +3,9 @@ import UIKit
 //Creating your own class - allow you to create new types with properties and methods.
 /*Classes have 5 difference:
 1. If you have parameters in your class you must always create your own initializer
+2. You can create a class based on an existing class – it inherits all the properties and methods of the original class, and can add its own on top.
  
+
 */
 
 class Dog {
@@ -16,6 +18,13 @@ class Dog {
     }
 }
 let frenchie = Dog(name: "Rocky", breed: "French Bulldog")
+
+class BorderCollie: Dog {
+    init(name: String){
+        super.init(name: name, breed: "BorderColli")
+    }
+}
+let borderCollie = BorderCollie(name: "Roger")
 
 
 class BoardGame {
@@ -46,10 +55,72 @@ class ThemePark {
         self.entryPrice = rides.count * 2
     }
 }
-let carousel = ThemePark(rides: "Cars")
+let carousel = ThemePark(rides: ["Cars", "Evil Wheel"])
 
 class Empty { }
 let nothing = Empty()
 
+
+//Class inheritance
+
+class Handbag {
+    var price: Int
+    init(price: Int) {
+        self.price = price
+    }
+}
+
+class DesignerHandbag: Handbag {
+    var brand: String
+    init(brand: String, price: Int) {
+        self.brand = brand
+        super.init(price: price)
+    }
+}
+
+
+class Bicycle {
+    var color: String
+    init(color: String) {
+        self.color = color
+    }
+}
+class MountainBike: Bicycle {
+    var tireThickness: Double
+    init(color: String, tireThickness: Double) {
+        self.tireThickness = tireThickness
+        super.init(color: color)
+    }
+}
+
+
+class Instrument {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+class Piano: Instrument {
+    var isElectric: Bool
+    init(isElectric: Bool) { // you don't need to init the param from super class if you are giving a value in super.init(param: paramType)
+        self.isElectric = isElectric
+        super.init(name: "Piano")
+    }
+}
+
+
+class Shape {
+    var sides: Int
+    init(sides: Int) {
+        self.sides = sides
+    }
+}
+class Rectangle: Shape {
+    var color: String
+    init(color: String) {
+        self.color = color
+        super.init(sides: 4)
+    }
+}
 
 
