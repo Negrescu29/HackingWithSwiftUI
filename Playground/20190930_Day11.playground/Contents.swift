d590083697fdd454d7ff5e1142ca857ab29b1b96
negrescu29 <ion.negrescu94@gmail.com>
2019-10-03 23:09:59 +0100

    Protocols and extensions summary

--- Playground/20190930_Day11.playground/Contents.swift
+++ Playground/20190930_Day11.playground/Contents.swift
@@ -187,7 +187,14 @@ struct User1: Identifiable1 {
 let twostraws = User1(id: "twostraws")
 twostraws.identify()
 
-
+//Protocols and extensions summary
+/*
+1. Protocols describe what methods and properties a conforming type must have, but don’t provide the implementations of those methods.
+2. You can build protocols on top of other protocols, similar to classes.
+3. Extensions let you add methods and computed properties to specific types such as Int.
+4. Protocol extensions let you add methods and computed properties to protocols.
+5. Protocol-oriented programming is the practice of designing your app architecture as a series of protocols, then using protocol extensions to provide default method implementations.
+ */
 
 
 
