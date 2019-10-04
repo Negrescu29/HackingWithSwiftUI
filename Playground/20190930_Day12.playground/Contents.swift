e9a701a15b8738abec1c823a4914cc909e129290
negrescu29 <ion.negrescu94@gmail.com>
2019-10-04 23:18:45 +0100

    Optional try

--- Playground/20190930_Day12.playground/Contents.swift
+++ Playground/20190930_Day12.playground/Contents.swift
@@ -163,4 +163,36 @@ func albumReleased(in year: Int) -> String? {
 }
 let album = albumReleased(in: 2006)?.uppercased()
 
+// Optional try
 
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+// try?, and changes throwing functions into functions that return an optional. If the function throws an error you’ll be sent nil as the result, otherwise you’ll get the return value wrapped as an optional.Using try? we can run checkPassword() like this:
+
+if let result = try? checkPassword("password") {
+    print("Result was \(result)")
+} else {
+    print("D'oh.")
+}
+
+//try!, which you can use when you know for sure that the function will not fail. If the function does throw an error, your code will crash. Using try! we can rewrite the code to this:
+
+try! checkPassword("sekrit")
+print("OK!")
