8af5895089a0525e76eecb54284a5cb5e4d10e49
negrescu29 <ion.negrescu94@gmail.com>
2019-10-04 22:18:54 +0100

    Unwrapping optionals
     - Optional strings might contain a string like “Hello” or they might be nil – nothing at all.

--- Playground/20190930_Day12.playground/Contents.swift
+++ Playground/20190930_Day12.playground/Contents.swift
@@ -4,3 +4,22 @@ import UIKit
 var age: Int? = nil
 var name: String?
 
+// Unwrapping optionals - Optional strings might contain a string like “Hello” or they might be nil – nothing at all.
+
+var nume: String? = "ION"
+
+if let unwrapped = nume {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
+
+var favoriteMovie: String? = nil
+favoriteMovie = "The Life of Brian"
+if let movie = favoriteMovie {
+    print("Your favorite movie is \(movie).")
+} else {
+    print("You don't have a favorite movie.")
+}
+
+
