77684c37a7fa7ca3a488b79a3bd58946214f8ae3
Ion <ion.negrescu@rubicoin.com>
2019-10-09 15:10:52 +0100

    Creating project WeSplit

---
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ion Negrescu on 09/10/2019.
+//  Copyright © 2019 Ion Negrescu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
